d372023345e00b10a300dc5991150b462d2832fc
kovacstomi121 <97965239+kovacstomi121@users.noreply.github.com>
2023-11-26 12:49:55 +0100

    Update page.tsx

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -43,7 +43,6 @@ const HomePage: React.FC<HomePageProps> = async ({ searchParams }) => {
               {products.length === 0 && <NoResults />}
 
               <div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                =======
                 {products.map((item) => (
                   <ProductCard key={item.id} data={item} />
                 ))}
