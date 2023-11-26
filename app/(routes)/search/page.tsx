4a9414e0c8756412044f59e7bc7358494d179ddd
kovacstomi121 <97965239+kovacstomi121@users.noreply.github.com>
2023-11-26 11:29:31 +0100

    Update page.tsx

--- app/(routes)/search/page.tsx
+++ app/(routes)/search/page.tsx
@@ -45,7 +45,7 @@ const SearchPage: React.FC<SearchPageProps> = ({ searchParams }) => {
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
             <div className="mt-6 lg:col-span-4 lg:mt-0">
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 {products.map((item) => (
                   <ProductCard key={item.id} data={item} />
                 ))}
