5a58dd739415e61d718a9d7f233b44e191b0542a
kovacstomi121 <kovacstomi121@gmail.com>
2023-11-27 17:44:16 +0100

    Frissítés

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -28,7 +28,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
 
   // A termékoldal JSX struktúrája
   return (
-    <div className="bg-white">
+    <div className="bg-white" style={{ backgroundColor: "#99B080" }}>
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
